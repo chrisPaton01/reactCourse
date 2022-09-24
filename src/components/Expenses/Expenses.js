a5ea7f193f9b25f9b8b92bd82e049844c707e770
chrispaton01 <christian.paton01@gmail.com>
2022-09-24 11:08:04 +0100

    assignment 2 changes

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,37 +1,53 @@
 import './Expenses.css';
 import ExpenseItem from './ExpenseItem';
+import ExpensesFilter from './ExpensesFilter';
 // ../ goes back one folder when searching through directories for imports
 import Card from '../UI/Card';
+import React, { useState } from 'react';
 
 // Apps.js contains import Expenses from './components/Expenses'; and
 // <Expenses expenses={expenses} /> to give arrays objects as props to
 // Expenses.js
 
-function Expenses(props) {
+const Expenses = (props) => {
+  const [filterYearState, setfilterYear] = useState('2019');
+
+  const showYearHandler = (selectedYear) => {
+    setfilterYear(selectedYear);
+    console.log('Expenses.JS working');
+    console.log(selectedYear + ' selectded');
+  };
+
   return (
-    <Card className='expenses'>
-      <ExpenseItem
-        title={props.expenses[0].title}
-        amount={props.expenses[0].amount}
-        date={props.expenses[0].date}
-      />
-      <ExpenseItem
-        title={props.expenses[1].title}
-        amount={props.expenses[1].amount}
-        date={props.expenses[1].date}
-      />
-      <ExpenseItem
-        title={props.expenses[2].title}
-        amount={props.expenses[2].amount}
-        date={props.expenses[2].date}
-      />
-      <ExpenseItem
-        title={props.expenses[3].title}
-        amount={props.expenses[3].amount}
-        date={props.expenses[3].date}
-      />
-    </Card>
+    <div>
+      <Card className='expenses'>
+        <ExpensesFilter
+          selected={filterYearState}
+          onFilterYearSelect={showYearHandler}
+        />
+        <ExpenseItem
+          title={props.expenses[0].title}
+          amount={props.expenses[0].amount}
+          date={props.expenses[0].date}
+        />
+        <ExpenseItem
+          title={props.expenses[1].title}
+          amount={props.expenses[1].amount}
+          date={props.expenses[1].date}
+        />
+        <ExpenseItem
+          title={props.expenses[2].title}
+          amount={props.expenses[2].amount}
+          date={props.expenses[2].date}
+        />
+        <ExpenseItem
+          title={props.expenses[3].title}
+          amount={props.expenses[3].amount}
+          date={props.expenses[3].date}
+        />
+      </Card>
+    </div>
   );
-}
+};
 
 export default Expenses;
--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -3,7 +3,7 @@ import './Card.css';
 function Card(props) {
   const classes = 'card ' + props.className;
   // Children prop is a reserved keyword, the value of the children prop will
-  // always be the content between the opening and closing tag of the Card 
+  // always be the content between the opening and closing tag of the Card
   // custom component element
   return <div className={classes}>{props.children}</div>;
 }
