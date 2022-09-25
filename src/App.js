f270cbbe78e21d59b9e46376783e8d442c375534
chrispaton01 <christian.paton01@gmail.com>
2022-09-25 14:02:58 +0100

    up to section 5 part 67

--- src/App.js
+++ src/App.js
@@ -1,47 +1,50 @@
 // importing ExpenseItem component from the components folder
 import Expenses from './components/Expenses/Expenses';
 import NewExpense from './components/NewExpense/NewExpense';
+import React, { useState } from 'react';
+
+// Array of expense item objects with various keys and values (obj properties)
+const dummyExpenses = [
+  {
+    id: 'e1',
+    title: 'Toilet Paper',
+    amount: 94.12,
+    // Date method is an in-built date constructor built into JS
+    date: new Date(2020, 7, 14),
+  },
+  {
+    id: 'e2',
+    title: 'New TV',
+    amount: 799.49,
+    date: new Date(2021, 2, 12),
+  },
+  {
+    id: 'e3',
+    title: 'Car Insurance',
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: 'e4',
+    title: 'New Desk (Wooden)',
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
 
 // This arrow function is a JS coded function itself, everything within is JSX
 const App = () => {
-  // Array of expense item objects with various keys and values (obj properties)
-  const expenses = [
-    {
-      id: 'e1',
-      title: 'Toilet Paper',
-      amount: 94.12,
-      // Date method is an in-built date constructor built into JS
-      date: new Date(2020, 7, 14),
-    },
-    {
-      id: 'e2',
-      title: 'New TV',
-      amount: 799.49,
-      date: new Date(2021, 2, 12),
-    },
-    {
-      id: 'e3',
-      title: 'Car Insurance',
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: 'e4',
-      title: 'New Desk (Wooden)',
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ];
+  const [expenses, setExpenses] = useState(dummyExpenses);
 
   const addExpenseHandler = (saveExpenseData) => {
-    console.log('In App.js');
-    console.log(saveExpenseData);
+    setExpenses((prevExpenses) => {
+      return [saveExpenseData, ...prevExpenses];
+    });
   };
 
   return (
     <div>
-      <h2>Let's get started!</h2>
-
+      {/*<h2>Let's get started!</h2>*/}
       {/* Using the imported ExpenseItem component as an element. This custom
       element will display any content contained with the ExpenseItem component
       The custom element was then given props from the values stored in the 
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -14,9 +14,32 @@ const Expenses = (props) => {
 
   const showYearHandler = (selectedYear) => {
     setfilterYear(selectedYear);
-    console.log('Expenses.JS working');
     console.log(selectedYear + ' selectded');
   };
+
+  // array function returns a new array
+  const filteredExpenses = props.expenses.filter((expense) => {
+    return expense.date.getFullYear().toString() === filterYearState;
+  });
+
+  let noExpensesContentMessage = <p>No expenses found.</p>;
+
+  /* .map() creates a new array based on another array and lets you 
+    transform every element in the original array. This code makes it so
+    that the items shown in the UI is based on the items present in the 
+    mapped array 
+    */
+  if (filteredExpenses.length > 0) {
+    noExpensesContentMessage = filteredExpenses.map((expenses) => (
+      // key is used as an unique id to help react identify each data set
+      <ExpenseItem
+        key={expenses.id}
+        title={expenses.title}
+        amount={expenses.amount}
+        date={expenses.date}
+      />
+    ));
+  }
 
   return (
     <div>
@@ -25,7 +48,21 @@ const Expenses = (props) => {
           selected={filterYearState}
           onFilterYearSelect={showYearHandler}
         />
-        <ExpenseItem
+        {noExpensesContentMessage}
+
+        {/* 
+        {filteredExpenses.length === 0 && <p>No expenses found.</p>}
+        {filteredExpenses.length > 0 && filteredExpenses.map((expenses) => (
+            <ExpenseItem
+              key={expenses.id}
+              title={expenses.title}
+              amount={expenses.amount}
+              date={expenses.date}
+            />
+          ))}
+        
+        */}
+        {/*<ExpenseItem
           title={props.expenses[0].title}
           amount={props.expenses[0].amount}
           date={props.expenses[0].date}
@@ -44,7 +81,7 @@ const Expenses = (props) => {
           title={props.expenses[3].title}
           amount={props.expenses[3].amount}
           date={props.expenses[3].date}
-        />
+        /> */}
       </Card>
     </div>
   );
