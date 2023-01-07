d31280e4e23205571c2f54217c9bb3290b206930
chrispaton01 <christian.paton01@gmail.com>
2023-01-07 22:44:01 +0000

    filter fixes with .map need to be implemented

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,5 +1,6 @@
 import './Expenses.css';
 import ExpensesList from './ExpensesList';
+import ExpenseItem from './ExpenseItem';
 import ExpensesFilter from './ExpensesFilter';
 // ../ goes back one folder when searching through directories for imports
 import Card from '../UI/Card';
@@ -10,7 +11,7 @@ import React, { useState } from 'react';
 // Expenses.js
 
 const Expenses = (props) => {
-  const [filterYearState, setfilterYear] = useState('2019');
+  const [filterYearState, setfilterYear] = useState('2020');
 
   const showYearHandler = (selectedYear) => {
     setfilterYear(selectedYear);
@@ -49,30 +50,22 @@ const Expenses = (props) => {
           selected={filterYearState}
           onFilterYearSelect={showYearHandler}
         />
-        {/* noExpensesContentMessage */}
+        {props.items.map((expense) => (
+          <ExpenseItem
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
 
-        <ExpensesList expenses={filteredExpenses}/>
+        <ExpensesList expenses={filteredExpenses} />
 
         {/*<ExpenseItem
           title={props.expenses[0].title}
           amount={props.expenses[0].amount}
           date={props.expenses[0].date}
         />
-        <ExpenseItem
-          title={props.expenses[1].title}
-          amount={props.expenses[1].amount}
-          date={props.expenses[1].date}
-        />
-        <ExpenseItem
-          title={props.expenses[2].title}
-          amount={props.expenses[2].amount}
-          date={props.expenses[2].date}
-        />
-        <ExpenseItem
-          title={props.expenses[3].title}
-          amount={props.expenses[3].amount}
-          date={props.expenses[3].date}
-        /> */}
+       */}
       </Card>
     </div>
   );
